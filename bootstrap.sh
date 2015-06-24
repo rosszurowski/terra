433c1517d5b3e8b7b7d8ff1100d7ebd42eb9c76b
Ross Zurowski <ross@rosszurowski.com>
2015-06-24 01:05:40 -0700

    Provisioning improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,8 @@ GRAY="\e[2m"
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
 END="\033[0m"
-SRC="https://github.com/rosszurowski/terra/tarball/master"
+REPO="rosszurowski/terra"
+SRC="https://github.com/${REPO}/tarball/master"
 
 # Download folder
 wget --no-check-certificate https://github.com/rosszurowski/server/tarball/master -qO - | tar -xz &&
@@ -14,6 +15,12 @@ cd rosszurowski-terra-*/ &&
 cp -f bin/* /usr/local/bin/ &&
 stack provision/base &&
 
+# Re-download the directory from the git repo
+cd .. &&
+rm -rf rosszurowski-terra-* &&
+git clone https://github.com/rosszurowski/terra --quiet &&
+cd terra &&
+
 # Close it up
 echo -e "" &&
 echo -e "   Base system provision applied" &&
@@ -22,4 +29,5 @@ echo -e "   Root login has been disabled for security purposes." &&
 echo -e "   Be sure to create a user via the following command:" &&
 echo -e "" &&
 echo -e "       new user ${GRAY}<username>${END}" &&
+echo -e ""
 echo -e ""
